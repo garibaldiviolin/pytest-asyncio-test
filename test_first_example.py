8cb6e9d690b0ea362170c15ec621edd10bbe7516
neto <garibaldiviolin@gmail.com>
2020-02-25 13:19:33 -0300

    Initial commit

---
+++ test_first_example.py
@@ -0,0 +1,20 @@
+import asyncio
+
+import pytest
+
+
+async def wait_one_second():
+    await asyncio.sleep(1)
+    return 1
+
+
+@pytest.mark.asyncio
+async def test_wait_one_second_with_pytest():
+    res = await wait_one_second()
+    assert res == 1
+
+
+def test_wait_one_second_without_pytest():
+    event_loop = asyncio.get_event_loop()
+    res = event_loop.run_until_complete(wait_one_second())
+    assert res == 1
